8838807e34a384efc85d4f03b4429f2365ddfef4
Ryan Bowden <ryanbowden1@outlook.com>
2016-11-11 15:41:10 +0000

    Added the server javascript file

--- index.js
+++ index.js
@@ -1,17 +1,116 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname +'/index.html');
-});
+app.get('/style.css', function (req, res) {
+  res.sendFile( __dirname + "/public/" + "style.css" );
+})
+
+app.get('/client.js', function (req, res) {
+  res.sendFile( __dirname + "/public/" + "client.js" );
+})
+
+app.get('/', function (req, res) {
+  res.sendFile( __dirname + "/public/" + "index.html" );
+})
+
+var showResults = false
+var cards = [];
+
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log('Client Connected');
+  for(var i=0; i<cards.length; i++) {
+      var item = cards[i];
+      var json = JSON.stringify({ text: item.name, category: item.category, id: i, votes: item.votes });
+      console.log("Sending to clients: " + json);
+      io.emit('retro card', json);
+      json = JSON.stringify({type: 'showresults'});
+      console.log("Sending to clients: " + json);
+      io.emit('retro card', json);
+  }
+
+  socket.on('retro card', function(msg){
+    console.log("Received: " + msg);
+
+    var obj = JSON.parse(msg);
+
+    if(typeof obj.type != 'undefined'){
+      switch (obj.type) {
+        case 'updatecategory':
+          updatedCategory(obj.id, obj.category);
+          break;
+      
+        default:
+          break;
+      }
+
+      return;
+    }
+
+    if(typeof obj.showResults != 'undefined'){
+      var json = JSON.stringify({type: 'showresults'});
+      console.log("Sending to clients: " + json);
+      io.emit('retro card', json);
+    }
+
+    //If a vote feature
+    if(typeof obj.voted != 'undefined'){
+      var id = obj.id;
+      console.log(obj.voted + ' = '+ cards[id].votes);
+
+      votes = cards[id].votes + 1;
+
+      cards[id].votes = votes;
+
+      var json = JSON.stringify({id: id, votes: votes });
+      console.log("Sending to clients: " + json);
+      io.emit('retro card', json);
+
+      return;
+    }
+
+    //work out what the user sent to us
+    if(typeof obj.text != 'undefined'){
+      var text = obj.text;
+      var category = obj.category;
+      var id = obj.id;
+      var votes = 0;
+
+      if (typeof id == 'undefined') {
+        id = addCardToArray(text, category);
+      } else {
+        cards[id] = {name: text, category: category};
+      }
+
+      var json = JSON.stringify({ text: text, category: category, id: id, votes: votes });
+      console.log("Sending to clients: " + json);
+      io.emit('retro card', json);
+    }
+  });
+  socket.on('disconnect', function(){
+    console.log('Client Disconnected');
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function updatedCategory(id, category){
+  cards[id].category = category;
+  var text = cards[id].name;
+  var votes = cards[id].votes;
+
+  var json = JSON.stringify({ text: text, category: category, id: id, votes: votes });
+  console.log("Sending to clients: " + json);
+  io.emit('retro card', json);
+
+}
+
+
+function addCardToArray(text, category){
+  cards.push({name: text, category: category, votes: 0 });
+  return cards.length-1;  
+}
