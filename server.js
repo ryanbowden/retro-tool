853bebd462c720a089da9211363a7d6d446f666a
Ryan Bowden <ryanbowden1@outlook.com>
2017-02-19 17:10:13 +0000

    fixed the issue where the css and javascript do not load

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const INDEX = path.join(__dirname, '/public/index.html');
 
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
 
 const server = express()
-   .use((req, res) => res.sendFile(INDEX) )
-   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .get('/', function (req, res) { res.sendFile(INDEX) })
+    .use(express.static(__dirname + '/public'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
