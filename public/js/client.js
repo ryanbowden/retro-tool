0350e9e815bb02e0af6edb1f9a23961719bfc0dc
Ryan Bowden <ryanbowden1@outlook.com>
2017-03-01 07:14:43 +0000

    Cleaned up the client js file

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,11 @@ var socket = io();
 var categories = ['What went well?', 'What could have gone better'];
 var showResultsAlways = false;
 
+/**
+ * Function addCardToBoard
+ * This adds the card to the board
+ * Returns nothing
+ */
 function addCardToBoard(id,text){
 	//Check card is not already on that page
 	$("#myitems").append(
@@ -13,16 +18,24 @@ function addCardToBoard(id,text){
 }
 
 $(function () {
+	/**
+	 * Add event listner to the submit buttom
+	 */
 	$('form').on('submit', function (e) {
 		e.preventDefault();
+		// Check a user does want to submit
 		if (!window.confirm("Are you sure you want to post this?")) {
 			return;
 		}
+		//Get the message and check it is not empty
 		var text = $("#description").val();
 		if (text == '') {
 			return;
 		}
 
+		//TODO: Make sure the text is below the limit the database can handle
+
+		//Send the text to the server so the card can be added to the board
 		socket.emit('retro card', text);
 	})
 });
