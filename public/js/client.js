5a33311ce9548c54d7d07d18b5b56938ef3ab66d
Ryan Bowden <ryanbowden1@outlook.com>
2017-03-02 13:23:29 +0000

    Fixed a few issues with link and made them clickable

--- public/js/client.js
+++ public/js/client.js
@@ -8,6 +8,8 @@ var showResultsAlways = false;
  * Returns nothing
  */
 function addCardToBoard(id,text){
+	//Check for links and convert them
+	text = linkify(text);
 	//Check card is not already on that page
 	$("#myitems").append(
 		$("<li>")
@@ -15,6 +17,24 @@ function addCardToBoard(id,text){
 		.addClass("draggable")
 		.html(text)
 	);
+}
+
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
 }
 
 $(function () {
